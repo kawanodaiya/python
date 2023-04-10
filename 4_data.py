dae9608ca400941c754d23f2953474ef94de1d8f
kawanodaiya <c612001234@tokushima-u.ac.jp>
2023-04-10 22:57:13 +0900

    4/10 commit

--- 4_data.py
+++ 4_data.py
@@ -16,3 +16,30 @@ print(to_split)
 
 x = '##'.join(to_split)
 print(x)
+
+y = r.copy()
+y[0] = 100
+print("y = ", y)
+print("r = ", r)
+
+num_tuple = (10, 20)
+print(num_tuple)
+
+x, y = num_tuple
+print(x, y)
+
+x, y = 10, 20
+print(x, y)
+
+# i = 10
+# j = 20
+# tmp = i
+# i = j
+# j = tmp
+# print(i, j)
+
+a = 100
+b = 200
+print(a, b)
+a, b = b, a
+print(a, b)
