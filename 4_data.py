e46a4734150c759a784f5350a188039fd15767f6
kawanodaiya <c612001234@tokushima-u.ac.jp>
2023-04-11 22:38:56 +0900

    4/11 commit

--- 4_data.py
+++ 4_data.py
@@ -1,3 +1,4 @@
+# リスト型
 r = [1, 2, 3, 4, 5, 1, 2, 3]
 
 print(r.index(3, 3))
@@ -31,15 +32,48 @@ print(x, y)
 x, y = 10, 20
 print(x, y)
 
-# i = 10
-# j = 20
-# tmp = i
-# i = j
-# j = tmp
-# print(i, j)
+#タプル型
+"""
+i = 10
+j = 20
+tmp = i
+i = j
+j = tmp
+print(i, j)
+"""
 
 a = 100
 b = 200
 print(a, b)
 a, b = b, a
 print(a, b)
+
+# 辞書型
+# x1 = {'a' : 1}
+# y1 = x1
+# y1['a'] = 1000
+# print(x1)
+# print(y1)
+
+x1 = {'a' : 1}
+y1 = x1.copy()
+y1['a'] = 1000
+print(x1)
+print(y1)
+
+fruits = {
+    'apple':100,
+    'banana':200,
+    'orange':300,
+}
+print(fruits['apple'])
+
+# 集合型
+my_friends = {'A', 'C', 'D'}
+A_friends = {'B', 'D', 'E', 'F'}
+print(my_friends & A_friends)
+print(my_friends | A_friends)
+
+f = ['apple', 'banana', 'apple', 'banana']
+kind = set(f)
+print(kind)
