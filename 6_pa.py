e3af2a8bcd146daa22fe483e91570c2209bad085
kawanodaiya <c612001234@tokushima-u.ac.jp>
2023-04-17 23:31:44 +0900

    4/17 commit

--- 6_pa.py
+++ 6_pa.py
@@ -5,3 +5,52 @@ except:
 
 r = package6.utils.say_twice('hello')
 print(r)
+
+# 組み込み関数
+import builtins
+
+builtins.print()
+
+ranking = {
+    'A': 100,
+    'B': 85,
+    'C': 95
+}
+
+print(sorted(ranking, key=ranking.get, reverse=True))
+
+# 標準ライブラリ
+s = "asdfghjkjhgfrtgb"
+
+d = {}
+for c in s:
+    if c not in d:
+        d[c] = 0
+    d[c] += 1
+print(d)
+
+d = {}
+for c in s:
+    d.setdefault(c, 0)
+    d[c] += 1
+print(d)
+
+from collections import defaultdict
+
+d = defaultdict(int)
+for c in s:
+    d[c] += 1
+print(d)
+
+# importする際の記述の仕方
+import collections
+import os
+import sys
+
+# import termcolor
+
+# import lesson_package
+
+# import config
+
+print(__name__)
--- package6/utils.py
+++ package6/utils.py
@@ -1,2 +1,4 @@
 def say_twice(word):
     return (word + '!') * 2
+
+print('utils:', __name__)
