2c7e51ca1267f9125c3d7a398e991c2d787c99d9
kawanodaiya <c612001234@tokushima-u.ac.jp>
2023-04-22 23:40:45 +0900

    4/22 commit

--- 7_cl.py
+++ 7_cl.py
@@ -19,23 +19,77 @@ del person
 
 print('##########')
 
+class Person(object):
+    def __init__(self, age=1):
+        self.age = age
+    def drive(self):
+        if self.age >= 18:
+            print('ok')
+        else:
+            raise Exception('no')
+
+class Baby(Person):
+    def __init__(self, age=1):
+        if age < 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+class Adult(Person):
+    def __init__(self, age=18):
+        if age >= 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+baby = Baby()
+adult = Adult()
+
 class Car(object):
+    def __init__(self, model=None):
+        self.model = model
     def run(self):
         print('run')
+    def ride(self, person):
+        person.drive()
 
 class ToyotaCar(Car):
     pass
 
 class TeslaCar(Car):
+    def __init__(self, model='model S',
+                enable_auto_run=False,
+                password='123'):
+        # 親の__init__を実行 self.model = model
+        super().__init__(model)
+        self._enable_auto_run = enable_auto_run
+        self.password = password
+    
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        if self.password == '456':
+            self._enable_auto_run = is_enable
+        else:
+            raise ValueError
+    
+    def run(self):
+        print('super fast')
     def auto_run(self):
         print('auto run')
 
 car = Car()
+car.ride(adult)
 car.run()
 print('##########')
-toyota_car = ToyotaCar()
+toyota_car = ToyotaCar('Lexus')
+print(toyota_car.model)
 toyota_car.run()
 print('##########')
-tesla_car = TeslaCar()
+tesla_car = TeslaCar('Model S', password='456')
+tesla_car.enable_auto_run = True
+print(tesla_car.model)
+print(tesla_car.enable_auto_run)
 tesla_car.run()
 tesla_car.auto_run()
