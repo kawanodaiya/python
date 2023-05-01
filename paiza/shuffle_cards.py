0c60e2b48f38755972c8fcb64db583ba0b59b405
kawanodaiya <c612001234@tokushima-u.ac.jp>
2023-05-01 15:58:04 +0900

    5/1 commit

--- paiza/shuffle_cards.py
+++ paiza/shuffle_cards.py
@@ -3,11 +3,12 @@ def shuffle_cards(n, m, k):
     for _ in range(k):
         # M枚ごとのセットに分ける
         sets = []
-        for i in range(0, n, m):
-            sets.append(cards[i:i+m])
+        for i in range(0, n, m): # range(start, stop, step)
+            sets.append(cards[i:i+m]) # 配列の追加　cardsのi番目の要素からi+m-1番目の要素までをスライスして取得する
         # セットごとに並び替える
         sets.reverse()
         # 並び替えたカードをリストに戻す
+        # 各セットsの中のカードcを一つずつ取り出して、新しいリストcardsに追加する
         cards = [c for s in sets for c in s]
     return cards
 
